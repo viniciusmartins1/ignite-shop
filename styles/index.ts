9f9ce688fd91f3c564ed39403f9d44675decaece
Vinicius Martins <vm3090474@gmail.com>
2023-07-19 18:05:06 -0300

    feat: Estrutura visual - Configurando Stitches

---
+++ styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  globalCss,
+  css,
+  createTheme,
+  getCssText,
+  keyframes,
+  theme,
+  styled,
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: "#8257e6",
+    },
+  },
+});
