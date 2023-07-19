b9a6c0f246ae4341b202ba7382734afd66d0d4cb
Vinicius Martins <vm3090474@gmail.com>
2023-07-19 20:26:25 -0300

    feat: Estrutura visual - Header e Container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import { globalStyles } from "../styles/global";
 
+import logoImg from "../assets/logo.svg";
+import { Container, Header } from "../styles/pages/app";
+
 globalStyles();
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
