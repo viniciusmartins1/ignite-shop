3d94302ae8c83d7803be24b2d0c61d92c1fe9548
Vinicius Martins <vm3090474@gmail.com>
2023-07-21 15:37:25 -0300

    feat: Produto & Checkout - Estrutura de sucesso01

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+import { ImageContainer } from "../styles/pages/success";
+import { SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuu! <strong>Diego Fernandes</strong>, sua{" "}
+        <strong>Camisa Beyond the Limits</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  );
 }
