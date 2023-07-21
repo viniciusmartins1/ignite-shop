62394780237d3209c43fba113e5cbe3cc84e559b
Vinicius Martins <vm3090474@gmail.com>
2023-07-21 14:40:33 -0300

    feat: Produto e Checkout - checkout no stripe

---
+++ pages/api/checkout.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { stripe } from "../../lib/stripe";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const priceId = "price_1NVwO7JbWj17bO6oZ0mN86kf";
+  const successUrl = `${process.env.NEXT_URL}/success`;
+  const cancelUrl = `${process.env.NEXT_URL}/`;
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+    mode: "payment",
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+  });
+
+  return res.status(201).json({
+    checkoutUrl: checkoutSession,
+  });
+}
