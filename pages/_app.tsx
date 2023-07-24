eed23692bf157b76477b5d5609c3e84a078a48dd
Vinicius Martins <vm3090474@gmail.com>
2023-07-24 18:14:13 -0300

    style: estrutura visual de sacola de compras

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { globalStyles } from "../styles/global";
 
 import logoImg from "../assets/logo.svg";
-import { Container, Header } from "../styles/pages/app";
+import * as s from "../styles/pages/app";
 
 import { Handbag } from "phosphor-react";
 
@@ -11,15 +11,49 @@ globalStyles();
 
 export default function App({ Component, pageProps }) {
   return (
-    <Container>
-      <Header>
+    <s.Container>
+      <s.Header>
         <Image src={logoImg} alt="" />
         <button disabled>
           <span>1</span>
           <Handbag size={24} weight="bold" />
         </button>
-      </Header>
+      </s.Header>
       <Component {...pageProps} />
-    </Container>
+      <s.ModalChart modalIsOpen="true">
+        <h3>Sacola de compras</h3>
+        <s.ProductItems>
+          <li>
+            {/* <Image */}
+            <div></div>
+            <div>
+              <p>Camiseta Beyond the Limits</p>
+              <strong>R$ 89,90</strong>
+              <button>Remover</button>
+            </div>
+          </li>
+          <li>
+            {/* <Image */}
+            <div></div>
+            <div>
+              <p>Camiseta Beyond the Limits</p>
+              <strong>R$ 89,90</strong>
+              <button>Remover</button>
+            </div>
+          </li>
+        </s.ProductItems>
+        <s.PaymmentContainer>
+          <s.QuantityDetails>
+            <p>Quantidade</p>
+            <span>3 itens</span>
+          </s.QuantityDetails>
+          <s.TotalDetails>
+            <span>Valor total</span>
+            <p>R$ 270,00</p>
+          </s.TotalDetails>
+        </s.PaymmentContainer>
+        <s.CheckoutButton>Finalizar compra</s.CheckoutButton>
+      </s.ModalChart>
+    </s.Container>
   );
 }
